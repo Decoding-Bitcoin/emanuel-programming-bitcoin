1e3563289d7f7ac959e70ff500a06ba9af4076a1
emanuks <emanuels313@gmail.com>
2024-04-29 04:38:01 -0300

    feat: adding transactions

---
+++ pybtc/transaction.py
@@ -0,0 +1,204 @@
+import requests
+from io import BytesIO
+
+from helper import *
+
+
+class Tx:
+    def __init__(self, version, tx_ins, tx_outs, lock_time, testnet=False):
+        self.version = version
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.lock_time = lock_time
+        self.testnet = testnet
+
+    def __repr__(self):
+        tx_ins = ''
+        for tx_in in self.tx_ins:
+            tx_ins += tx_in.__repr__() + '\n'
+        tx_outs = ''
+
+        for tx_out in self.tx_outs:
+            tx_outs += tx_out.__repr__() + '\n'
+
+        return 'tx: {}\nversion: {}\ntx_ins: {}\ntx_outs: {}\nlock_time: {}\n'.format(
+            self.id(),
+            self.version,
+            tx_ins,
+            tx_outs,
+            self.lock_time
+        )
+
+    def id(self):
+        """Human-readable hexadecimal of the transaction hash"""
+        return self.hash().hex()
+
+    def hash(self):
+        """Binary hash of the legacy serialization"""
+        return hash256(self.serialize())[::-1]
+
+    def serialize(self):
+        """Returns the byte serialization of the transaction"""
+        result = int_to_little_endian(self.version, 4)
+
+        result += encode_varint(len(self.tx_ins))
+        for tx_in in self.tx_ins:
+            result += tx_in.serialize()
+
+        result += encode_varint(len(self.tx_outs))
+        for tx_out in self.tx_outs:
+            result += tx_out.serialize()
+
+        result += int_to_little_endian(self.lock_time, 4)
+        return result
+
+    def fee(self, testnet=False):
+        fee = 0
+        for tx_out in self.tx_outs:
+            fee += tx_out.amount
+
+        for tx_in in self.tx_ins:
+            fee -= tx_in.value(testnet)
+
+        return fee
+
+    @classmethod
+    def parse(cls, stream, testnet=False):
+        serialized_version = stream.read(4)
+        version = little_endian_to_int(serialized_version)
+
+        input_qty = read_varint(stream)
+        tx_ins = []
+        for n in range(input_qty):
+            tx_ins.append(TxIn.parse(stream))
+
+        output_qty = read_varint(stream)
+        tx_outs = []
+        for n in range(output_qty):
+            tx_outs.append(TxOut.parse(stream))
+
+        serialized_lock_time = stream.read(4)
+        lock_time = little_endian_to_int(serialized_lock_time)
+
+        return Tx(version, tx_ins, tx_outs, lock_time)
+
+
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig=None, sequence=0xffffffff):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        if script_sig is None:
+            self.script_sig = Script()
+        else:
+            self.script_sig = script_sig
+        self.sequence = sequence
+
+    def __repr__(self):
+        return '{}:{}'.format(self.prev_tx.hex(), self.prev_index)
+
+    def serialize(self):
+        """Returns the byte serialization of the transaction input"""
+        result = self.prev_tx[::-1]
+        result += int_to_little_endian(self.prev_index, 4)
+        result += self.script_sig.serialize()
+        result += int_to_little_endian(self.sequence, 4)
+        return result
+
+    def fetch_tx(self, testnet=False):
+        return TxFetcher.fetch(self.prev_tx.hex(), testnet)
+
+    def value(self, testnet=False):
+        """
+        Get the output value by looking up the tx hash
+        Returns the amount in satoshi
+        """
+        tx = self.fetch_tx(testnet)
+        return tx.tx_outs[self.prev_index].amount
+
+    def script_pubkey(self, testnet=False):
+        """
+        Get the ScriptPubKey by looking up the tx hash
+        Returns a Script object
+        """
+        tx = self.fetch_tx(testnet)
+        return tx.tx_outs[self.prev_index].script_pubkey
+
+    @classmethod
+    def parse(cls, stream, testnet=False):
+        serialized_tx_id = stream.read(32)
+        tx_id = little_endian_to_int(serialized_tx_id)
+
+        serialized_tx_index = stream.read(4)
+        tx_index = little_endian_to_int(serialized_tx_index)
+
+        script_sig = ''
+        script_sig_size = read_varint(stream)
+        for n in range(script_sig_size):
+            script_sig += bytes(stream.read(1))
+
+        serialized_sequence = stream.read(4)
+        sequence = little_endian_to_int(serialized_sequence)
+
+        return TxIn(tx_id, tx_index, script_sig, sequence)
+
+
+class TxOut:
+    def __init__(self, amount, script_pubkey):
+        self.amount = amount
+        self.script_pubkey = script_pubkey
+
+    def __repr__(self):
+        return '{}:{}'.format(self.amount, self.script_pubkey)
+
+    def serialize(self):
+        """Returns the byte serialization of the transaction output"""
+        result = int_to_little_endian(self.amount, 8)
+        result += self.script_pubkey.serialize()
+        return result
+
+    @classmethod
+    def parse(cls, stream, testnet=False):
+        serialized_amount = stream.read(8)
+        amount = little_endian_to_int(serialized_amount)
+
+        script_pubkey = ''
+        script_pubkey_size = read_varint(stream)
+        for n in range(script_pubkey_size):
+            script_pubkey += bytes(stream.read(1))
+
+        return TxOut(amount, script_pubkey)
+
+
+class TxFetcher:
+    cache = {}
+
+    @classmethod
+    def get_url(cls, testnet=False):
+        if testnet:
+            return 'http://testnet.programmingbitcoin.com'
+        else:
+            return 'http://mainnet.programmingbitcoin.com'
+
+    @classmethod
+    def fetch(cls, tx_id, testnet=False, fresh=False):
+        if fresh or (tx_id not in cls.cache):
+            url = '{}/tx/{}.hex'.format(cls.get_url(testnet), tx_id)
+            response = requests.get(url)
+            try:
+                raw = bytes.fromhex(response.text.strip())
+            except ValueError:
+                raise ValueError('Unexpected response: {}'.format(response.text))
+
+            if raw[4] == 0:
+                raw = raw[:4] + raw[6:]
+                tx = Tx.parse(BytesIO(raw), testnet)
+                tx.lock_time = little_endian_to_int(raw[-4:])
+            else:
+                tx = Tx.parse(BytesIO(raw), testnet)
+
+            if tx.id() != tx_id:
+                raise ValueError('Not the same id: {} vs {}'.format(tx.id(), tx_id))
+
+            cls.cache[tx_id] = tx
+        cls.cache[tx_id].testnet = testnet
+        return cls.cache[tx_id]
--- tests/helper_test.py
+++ tests/helper_test.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from io import BytesIO
 
 from pybtc.helper import *
 
@@ -23,3 +24,54 @@ class HelperTest(TestCase):
         le2 = 0x8DF5C116EB.to_bytes(18, 'little')
         self.assertEqual(little_endian_to_int(le2), n2)
         self.assertFalse(little_endian_to_int(le2) == n1)
+
+    def test_read_var_int(self):
+        n1 = 0x64.to_bytes(1)
+        vi1 = BytesIO(n1)
+        self.assertEqual(read_varint(vi1),100)
+
+        n2 = 0xfdff00.to_bytes(3)
+        vi2 = BytesIO(n2)
+        self.assertEqual(read_varint(vi2), 255)
+
+        n3 = 0xfd2b02.to_bytes(3)
+        vi3 = BytesIO(n3)
+        self.assertEqual(read_varint(vi3), 555)
+
+        n4 = 0xfe7f110100.to_bytes(5)
+        vi4 = BytesIO(n4)
+        self.assertEqual(read_varint(vi4), 70015)
+
+        n5 = 0xff6dc7ed3e60100000.to_bytes(9)
+        vi5 = BytesIO(n5)
+        self.assertEqual(read_varint(vi5), 18005558675309)
+
+        vi6 = BytesIO(n3)
+        self.assertFalse(read_varint(vi6) == 255)
+
+    def test_encode_varint(self):
+        n1 = 100
+        encoded_1 = 0x64.to_bytes(1)
+        self.assertEqual(encode_varint(n1), encoded_1)
+
+        n2 = 255
+        encoded_2 = 0xfdff00.to_bytes(3)
+        self.assertEqual(encode_varint(n2), encoded_2)
+
+        n3 = 555
+        encoded_3 = 0xfd2b02.to_bytes(3)
+        self.assertEqual(encode_varint(n3), encoded_3)
+
+        n4 = 70015
+        encoded_4 = 0xfe7f110100.to_bytes(5)
+        self.assertEqual(encode_varint(n4), encoded_4)
+
+        n5 = 18005558675309
+        encoded_5 = 0xff6dc7ed3e60100000.to_bytes(9)
+        self.assertEqual(encode_varint(n5), encoded_5)
+
+        self.assertFalse(encode_varint(n2) == n3)
+
+        with self.assertRaises(ValueError):
+            n6 = 0x10000000000000000
+            encode_varint(n6)
